9b8b7d98f1236216c935767f7c4ddb94183e52b7
Ritweek Ranjan <ritweek@users.noreply.github.com>
2021-04-20 18:19:12 +0530

    Update Startup.cs
    team city ci test

--- app.medicine.api/Startup.cs
+++ app.medicine.api/Startup.cs
@@ -24,6 +24,7 @@ namespace app.medicine.api
         public IConfiguration Configuration { get; }
 
         // This method gets called by the runtime. Use this method to add services to the container.
+        // test ci on team city
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddTransient<IMedicineService, MedicineService>();
